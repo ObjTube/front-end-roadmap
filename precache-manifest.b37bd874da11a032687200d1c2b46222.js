4613e8759f0d7e35bc1de673d0f517a99532607d
ObjTube <minyomi123@163.com>
2020-06-17 22:08:53 +0800

    Updates

---
+++ precache-manifest.b37bd874da11a032687200d1c2b46222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7868ebdc7425de92ec6dd19177925ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d7c582cab6bf668106",
+    "url": "/static/css/main.2f8b6b6f.chunk.css"
+  },
+  {
+    "revision": "bc86d76b0f70d13a8b16",
+    "url": "/static/js/2.b73b662e.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/static/js/2.b73b662e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d7c582cab6bf668106",
+    "url": "/static/js/main.2ec4d8c6.chunk.js"
+  },
+  {
+    "revision": "954898592ac8a2926b19",
+    "url": "/static/js/runtime-main.e8555bde.js"
+  }
+]);
